1e25ecacd0a34497e2e2013e01b985c6f7da0e70
invrtd <invrtd04.5@gmail.com>
2024-11-10 02:42:37 +0000

    most of groud work figured out, fine tuneing

--- app/boards/shields/nice_view/widgets/util.c
+++ app/boards/shields/nice_view/widgets/util.c
@@ -20,8 +20,10 @@ void rotate_canvas(lv_obj_t *canvas, lv_color_t cbuf[]) {
     img.header.h = CANVAS_SIZE;
 
     lv_canvas_fill_bg(canvas, LVGL_BACKGROUND, LV_OPA_COVER);
-    lv_canvas_transform(canvas, &img, 900, LV_IMG_ZOOM_NONE, -1, 0, CANVAS_SIZE / 2,
-                        CANVAS_SIZE / 2, true);
+    // lv_canvas_transform(canvas, &img, 900, LV_IMG_ZOOM_NONE, -1, 0, CANVAS_SIZE / 2,
+    //                    CANVAS_SIZE / 2, true);
+    lv_canvas_transform(canvas, &img, -900, LV_IMG_ZOOM_NONE, -1, 0, CANVAS_SIZE / 2,
+                        CANVAS_SIZE / 2 - 1, true);
 }
 
 void draw_battery(lv_obj_t *canvas, const struct status_state *state) {
